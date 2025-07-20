869d332ac84f9098f2c334c9cf1d3a054064e573
dimubro <163578777+dimubro@users.noreply.github.com>
2025-07-20 19:44:43 +0530

    Update refresh_token.php

--- refresh_token.php
+++ refresh_token.php
@@ -1,6 +1,6 @@
 <?php 
     $client_id = 'Client ID';
-    $redirect_uri = 'https://cleesecatering.com/site/quick_book/call-back.php'; // Must match what is registered in Intuit app
+    $redirect_uri = 'redirect url'; // You add the path to the callback.php file.
     $scope = 'com.intuit.quickbooks.accounting openid profile email phone address';
 
     $auth_url = 'https://appcenter.intuit.com/connect/oauth2?' . http_build_query([
@@ -8,10 +8,10 @@
         'redirect_uri' => $redirect_uri,
         'response_type' => 'code',
         'scope' => $scope,
-        'state' => 'random_string_here' // Optional, for security
+        'state' => 'random_string_here' 
     ]);
 
-    // Redirect to QuickBooks login
+    
     header("Location: $auth_url");
     exit;
 
